16485aa93b10fa7cbfca8d3740de813d68e6bcf7
IN-P <cooladam_@naver.com>
2025-02-21 14:19:29 +0900

    2025-02-21

--- java_basic/src/com/company/java005_ex/ForEx003_Up.java
+++ java_basic/src/com/company/java005_ex/ForEx003_Up.java
@@ -3,14 +3,17 @@ package com.company.java005_ex;
 public class ForEx003_Up {
 
 	public static void main(String[] args) {
-		int sum=0;
-		String result="";
-		for(int i=1;i<=10;i++) {
-			sum+=i;
-			result+=""+i;
-			if(i<10) {result+="+";}
-			}
-		System.out.println(result+"="+sum);
+//		int sum=0;
+//		String result="";
+//		for(int i=1;i<=10;i++) {
+//			sum+=i;
+//			result+=""+i;
+//			if(i<10) {result+="+";}
+//			}
+//		System.out.println(result+"="+sum);
+		
+		//삼항 연산자로 압축해보기
+		for(int i=1,sum=0;i<=10;i++) {sum+=i; System.out.print(i+(i==10?"="+sum:"+"));}
 	}
 
 }
--- java_basic/src/com/company/miniproject/Bank002.java
+++ java_basic/src/com/company/miniproject/Bank002.java
@@ -7,62 +7,49 @@ public class Bank002 {
 	public static void main(String[] args) {
 		System.out.println("WELCOME! (주)CODEJOHNS_BANK");
 		Scanner scan = new Scanner(System.in);
-		int input=0,age=0;
+		int age=0;
 		long cash=0;
-		String id="",pwd="",tempid="",temppwd="";
+		String id="",pwd="";
 		while(true) {
 			System.out.println("=====BANK=====\n* 1.추가\n* 2.조회\n* 3.입금\n* 4.출금\n* 5.삭제\n* 9.종료");
 			System.out.print("입력>>> ");
-			input=scan.nextInt();
+			int input=scan.nextInt();
+			
 			switch(input) {
 			case 1:
-				System.out.print("아이디 입력 : ");
-				id=scan.next();
-				System.out.print("비밀번호 입력 : ");
-				pwd=scan.next();
-				System.out.print("나이 입력 : ");
-				age=scan.nextInt();
-				System.out.print("잔액 입력 : ");
-				cash=scan.nextInt();
-				break;
-			case 2: 
-				System.out.print("\n아이디 입력 : ");
-				tempid=scan.next();
-				System.out.print("비밀번호 입력 : ");
-				temppwd=scan.next();
+				System.out.print("아이디 입력 : ");id=scan.next();
+				System.out.print("비밀번호 입력 : ");pwd=scan.next();
+				System.out.print("나이 입력 : ");age=scan.nextInt();
+				System.out.print("잔액 입력 : ");cash=scan.nextInt();
+			break;
+			
+			case 2,3,4,5: 
+				System.out.print("\n아이디 입력 : ");String tempid=scan.next();
+				System.out.print("비밀번호 입력 : ");String temppwd=scan.next();
 				if(!tempid.equals(id)||!temppwd.equals(pwd)) {System.out.println("다시 확인해주세요.");break;}
-				System.out.printf("==계좌조회\nID : %s\nPASS : %s\n나이 : %d\n잔액 : %d\n",id,pwd,age,cash);
+				
+				switch(input) {
+				case 2: 
+					System.out.printf("==계좌조회\nID : %s\nPASS : %s\n나이 : %d\n잔액 : %d\n",id,pwd,age,cash);
 				break;
-			case 3: 
-				System.out.print("\nid : ");
-				tempid=scan.next();
-				System.out.print("pass : ");
-				temppwd=scan.next();
-				if(!tempid.equals(id)||!temppwd.equals(pwd)) {System.out.println("다시 확인해주세요.");break;}
-				System.out.print("입금 : ");
-				cash+=scan.nextInt();
-				System.out.println("==입금완료\n잔액 : "+cash);
+				
+				case 3: 
+					System.out.print("입금 : ");cash+=scan.nextInt();
+					System.out.println("==입금완료\n잔액 : "+cash);
 				break;
-			case 4: 
-				System.out.print("\nid : ");
-				tempid=scan.next();
-				System.out.print("pass : ");
-				temppwd=scan.next();
-				if(!tempid.equals(id)||!temppwd.equals(pwd)) {System.out.println("다시 확인해주세요.");break;}
-				System.out.print("출금 : ");
-				cash-=scan.nextInt();
-				System.out.println("==출금완료\n잔액 : "+cash);
+				
+				case 4: 
+					System.out.print("출금 : ");cash-=scan.nextInt();
+					System.out.println("==출금완료\n잔액 : "+cash);
 				break;
-			case 5: 
-				System.out.print("\nid : ");
-				tempid=scan.next();
-				System.out.print("pass : ");
-				temppwd=scan.next();
-				if(!tempid.equals(id)||!temppwd.equals(pwd)) {System.out.println("다시 확인해주세요.");break;}
-				System.out.println("계좌를 삭제하시겠습니까? (Y/N)");
-				char select = scan.next().charAt(0);
-				if(select=='y'||select=='Y') {id="";pwd="";age=0;cash=0;}
+				
+				case 5: 
+					System.out.println("계좌를 삭제하시겠습니까? (Y/N)");char select = scan.next().charAt(0);
+					if(select=='y'||select=='Y') {id="";pwd="";age=0;cash=0;}
 				break;
+				}
+			break;
+			
 			case 9: scan.close(); return;
 			}
 		}
@@ -70,3 +57,13 @@ public class Bank002 {
 }
 
 ///입력,아이디,비번,나이,잔액
+///반복{
+///  제어1)입력
+///
+///  제어2,3,4,5){
+///	   (공통)아이디 비번 체크
+///	   (각번호제어)각처리할 일
+///	 }
+///
+///	 제어9)종료
+///}
